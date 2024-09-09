15a649c94060c5f7f24ca60b38987e434cfd7e4a
Corey Taylor <coreyblaketaylor@hotmail.com>
2024-09-09 13:46:21 +0100

    V1.2

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 This is a very basic text editor chrome extension project developed using html, css and javascript as a way for me to learn a bit of javascript.
 
-v1.11
-- Minor aethsetic improvements.
+V1.2
+- Added button descriptions that appear when hovered over for 1 second.
 
 Version History:
 
-v1.1
+V1.11
+- Minor aethsetic improvements.
+
+V1.1
 - Added the ability to copy textarea content to the clipboard.
 - Added the ability to change the colour of the notepad.
 
--- script.js
+++ script.js
@@ -1,22 +1,35 @@
+// Delceration of variables and binding event listeners to buttons.
 let saveStack = [];
 
-document.getElementById("undo").addEventListener("click", undo);
+const content = document.getElementById("editor");
+var timeoutIDsave;
+var timeoutIDdesc;
+
+document.getElementById("fileName").addEventListener("click", enterFileName);
 
 document.getElementById("save").addEventListener("click", save);
+document.getElementById("save").addEventListener("mouseover", function() {timeoutIDdesc = setTimeout(function() {info("save", "left");}, 1000);});
+document.getElementById("save").addEventListener("mouseout", function() {clear("save")});
 
-document.getElementById("title").addEventListener("click", clear);
+document.getElementById("undo").addEventListener("click", undo);
+document.getElementById("undo").addEventListener("mouseover", function() {timeoutIDdesc = setTimeout(function() {info("undo");}, 1000);});
+document.getElementById("undo").addEventListener("mouseout", function() {clear("undo")});
 
 document.getElementById("copy").addEventListener("click", copy);
-
-document.getElementById("red").addEventListener("click", function() {changeTheme("red")});
+document.getElementById("copy").addEventListener("mouseover", function() {timeoutIDdesc = setTimeout(function() {info("copy");}, 1000);});
+document.getElementById("copy").addEventListener("mouseout", function() {clear("copy")});
 
 document.getElementById("green").addEventListener("click", function() {changeTheme("green")});
+document.getElementById("green").addEventListener("mouseover", function() {timeoutIDdesc = setTimeout(function() {info("green", "right");}, 1000);});
+document.getElementById("green").addEventListener("mouseout", function() {clear("green");});
+
+document.getElementById("red").addEventListener("click", function() {changeTheme("red")});
+document.getElementById("red").addEventListener("mouseover", function() {timeoutIDdesc = setTimeout(function() {info("red", "right");}, 1000);});
+document.getElementById("red").addEventListener("mouseout", function() {clear("red")});
 
 document.getElementById("blue").addEventListener("click", function() {changeTheme("blue")});
-
-const content = document.getElementById("editor");
-
-var timeoutID;
+document.getElementById("blue").addEventListener("mouseover", function() {timeoutIDdesc = setTimeout(function() {info("blue", "right");}, 1000);});
+document.getElementById("blue").addEventListener("mouseout", function() {clear("blue")});
 
 function copy() 
 {   
@@ -33,23 +46,23 @@ function changeTheme(colour)
     {
         case "red":
             document.body.style.background = "rgb(139, 0, 0)";
-            document.getElementById("title").style.background = "rgb(139, 0, 0)";
+            document.getElementById("fileName").style.background = "rgb(139, 0, 0)";
             break;
         case "green":
             document.body.style.background = "rgb(39, 107, 27)";
-            document.getElementById("title").style.background = "rgb(39, 107, 27)";
+            document.getElementById("fileName").style.background = "rgb(39, 107, 27)";
             break;
         case "blue":
             document.body.style.background = "rgb(0, 98, 139)";
-            document.getElementById("title").style.background = "rgb(0, 98, 139)";
+            document.getElementById("fileName").style.background = "rgb(0, 98, 139)";
             break;
     }
 };
 
-function clear() 
+function enterFileName() 
 {
-    // Clears the title input field for convinience when clicked on.
-    document.getElementById("title").value = '';
+    // Clears the fileName input field for convinience when clicked on.
+    document.getElementById("fileName").value = "";
 };
 
 function save() 
@@ -90,11 +103,47 @@ function undo()
     }
 };
 
+function info(button, pos)
+{   
+    setTimeout(1000);
+    const buttonDesc = document.getElementById('description');
+    button = document.getElementById(button);
+    let buttonText = button.dataset.desc;
+    buttonDesc.style.borderStyle = "ridge";
+    /* Using getBoundingCLientRect method to get the position and size of the button so that the decritpion can float 
+    in a position relative to it.*/
+    const rect = button.getBoundingClientRect();
+    if (pos == "right")
+    {
+      offsetX = -150; 
+    }
+    else if(pos == "left")
+    {
+        offsetX = -40;
+    }
+    else
+    {
+      offsetX = -20;
+    }
+    buttonDesc.style.left = (rect.right + offsetX) + "px";
+    buttonDesc.textContent = buttonText;
+};
+
+function clear(button) 
+{
+    // Clears the textcontent of the corresponding button.
+    clearTimeout(timeoutIDdesc);
+    const buttonDesc = document.getElementById('description');
+    button = document.getElementById(button);
+    buttonDesc.style.borderStyle = "none";
+    buttonDesc.textContent = "";
+};
+
 content.addEventListener("input", function() 
 {
     // Timeout used to make sure that there is a 1 second delay after input before saving to prevent unnecessary save states.
-    clearTimeout(timeoutID);
-    timeoutID = setTimeout(function() {
+    clearTimeout(timeoutIDsave);
+    timeoutIDsave = setTimeout(function() {
         autoSave();
     }, 1000);
 });
